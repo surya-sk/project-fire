991d1a2b88a779f45bc38e93f0270b84328f196e
Surya Kashyap <surya.sk05@outlook.com>
2021-02-27 18:43:21 -0400

    Add jumping controls

--- Source/ProjectFire/ShooterCharacter.cpp
+++ Source/ProjectFire/ShooterCharacter.cpp
@@ -36,6 +36,7 @@ void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &AShooterCharacter::LookRight);
 	PlayerInputComponent->BindAxis(TEXT("LookUpRate"), this, &AShooterCharacter::LookUpRate);
 	PlayerInputComponent->BindAxis(TEXT("LookRightRate"), this, &AShooterCharacter::LookRightRate);
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &ACharacter::Jump);
 }
 
 void AShooterCharacter::MoveForward(float Axis)
