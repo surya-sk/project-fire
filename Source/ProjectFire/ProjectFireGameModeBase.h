8614fb0d5dc3f87d4302b39f5ace602ec1a7edd1
Surya Kashyap <surya.sk05@outlook.com>
2021-02-27 17:58:21 -0400

    Upload project files

---
+++ Source/ProjectFire/ProjectFireGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectFireGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTFIRE_API AProjectFireGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
