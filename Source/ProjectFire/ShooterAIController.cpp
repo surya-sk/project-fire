a8b08021324193b9d0f820cc6f8eb61efc913efd
Surya Kashyap <surya.sk05@outlook.com>
2021-03-07 09:45:02 -0400

    Get player location in Blackboard from C++

--- Source/ProjectFire/ShooterAIController.cpp
+++ Source/ProjectFire/ShooterAIController.cpp
@@ -3,14 +3,16 @@
 
 #include "ShooterAIController.h"
 #include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 void AShooterAIController::BeginPlay()
 {
 	Super::BeginPlay();
-
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 	if (AIBehavior != nullptr)
 	{
 		RunBehaviorTree(AIBehavior);
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
 	}
 }
 
